1cde06150650ef6022893962c0d669de801cdf6e
Bharath-6 <67066997+Bharath-6@users.noreply.github.com>
2024-05-14 14:41:16 +0530

    Done with book project where logging is applied

---
+++ src/main/java/com/example/Book/Service/BookService.java
@@ -0,0 +1,46 @@
+package com.example.Book.Service;
+
+import com.example.Book.Model.Book;
+import com.example.Book.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book getBookById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        return optionalBook.orElse(null);
+    }
+
+    public Book updateBook(Long id, Book updatedBook) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            Book existingBook = optionalBook.get();
+            existingBook.setTitle(updatedBook.getTitle());
+            existingBook.setAuthor(updatedBook.getAuthor());
+            return bookRepository.save(existingBook);
+        }
+        return null; // Or throw an exception if not found
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
